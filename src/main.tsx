f9bc330986e7e2a3accfe224ec5d65abeac855d1
petro1909 <petya-97_97@mail.ru>
2023-12-05 14:53:36 +0300

    feat: add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/app/index.tsx';
 import './index.css';
+import { ErrorBoundary } from 'react-error-boundary';
+import { ErrorFallback } from '@components/errorFallback/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<ErrorFallback />}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
