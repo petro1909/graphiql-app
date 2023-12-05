92a7e3e0651f714f57b2de6f89d85231fa7b463f
petro1909 <petya-97_97@mail.ru>
2023-12-05 14:18:41 +0300

    refactor: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 /// <reference types='vite/client'/>
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
@@ -10,6 +11,18 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@app_types': path.resolve(__dirname, './src/types'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@redux': path.resolve(__dirname, './src/redux'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@validation': path.resolve(__dirname, './src/validation'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
