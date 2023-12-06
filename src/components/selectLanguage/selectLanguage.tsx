451e233ee2de1b90f8ca311652b45aaf7a7f2bde
Darya <bobko.dv@gmail.com>
2023-12-06 10:22:27 +0100

    recactor: add active style, select

--- src/components/selectLanguage/selectLanguage.tsx
+++ src/components/selectLanguage/selectLanguage.tsx
@@ -3,10 +3,8 @@ import styles from './selectLanguage.module.scss';
 export const SelectLanguage: React.FC = () => {
   return (
     <div className={styles.field}>
-      <select className={styles.select} name="pets" id="pet-select">
-        <option value="English" selected>
-          English
-        </option>
+      <select className={styles.select} name="language" id="language-select" defaultValue="English">
+        <option value="English">English</option>
         <option value="Russian">Russian</option>
       </select>
     </div>
