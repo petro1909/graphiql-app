e2fb2469dc882b906aca652ccbc7fa408ac1e390
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-12-08 01:15:41 +0400

    fix: make husky executable

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -2,31 +2,26 @@ import './main-styles.scss';
 
 export function MainPage() {
   //Main GraphiQL Page
-  return(
+  return (
     <main>
       <section className="main-top">
         <div className="endpoint">
-          <input
-              id="endpoint-input"
-              className="input endpoint-input"
-              type="text"
-              placeholder='type the endpoint'
-            />
-            <button type="button" className="btn endpoint-btn" >
-              Set endpoint
-            </button>
-          </div>
-          <div className="documentation-btn">Doc explorer</div>
+          <input id="endpoint-input" className="input endpoint-input" type="text" placeholder="type the endpoint" />
+          <button type="button" className="btn endpoint-btn">
+            Set endpoint
+          </button>
+        </div>
+        <div className="documentation-btn">Doc explorer</div>
       </section>
       <section className="main-content">
-      <aside className="actions">actions</aside>
-      <section className="graphGL">
-      <div className="graphGL-request">request</div>
-      <div className="graphGL-result">responce</div>
-      <div className="graphGL-vars"></div>
-      <div className="graphGL-json"></div>
-      </section>
-      <div className="documentation-content">Doc content</div>
+        <aside className="actions">actions</aside>
+        <section className="graphGL">
+          <div className="graphGL-request">request</div>
+          <div className="graphGL-result">responce</div>
+          <div className="graphGL-vars"></div>
+          <div className="graphGL-json"></div>
+        </section>
+        <div className="documentation-content">Doc content</div>
       </section>
     </main>
   );
