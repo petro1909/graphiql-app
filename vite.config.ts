479342ff4d44125a9f059ee61cf6a5d9a838cf94
petro1909 <petya-97_97@mail.ru>
2023-12-08 11:34:19 +0300

    refactor: add aliases

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
       '@validation': path.resolve(__dirname, './src/validation'),
       '@localization': path.resolve(__dirname, './src/localization'),
       '@lang': path.resolve(__dirname, './lang'),
+      '@service': path.resolve(__dirname, './src/service'),
     },
   },
   test: {
