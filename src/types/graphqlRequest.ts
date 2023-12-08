bf2bfa05e674dc7c8dc91b06f9df093e356c08db
petro1909 <petya-97_97@mail.ru>
2023-12-08 11:28:21 +0300

    feat: create types for GraphQL requests

---
+++ src/types/graphqlRequest.ts
@@ -0,0 +1,14 @@
+type BaseGraphQLRequest = {
+  URL: string;
+  query: string;
+};
+
+export type GraphQLRawRequest = {
+  headers: string;
+  variables: string;
+} & BaseGraphQLRequest;
+
+export type GraphQLValidRequest = {
+  headers: Record<string, string | undefined>;
+  variables: Record<string, string | undefined>;
+} & BaseGraphQLRequest;
