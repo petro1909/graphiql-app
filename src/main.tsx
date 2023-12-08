3acf4de27d0bfe50f4a14c6f1c098801f4d912d3
petro1909 <petya-97_97@mail.ru>
2023-12-08 11:33:17 +0300

    feat: add store provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import './index.css';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '@components/errorFallback/index.tsx';
 import { LocaleProvider } from './localization/localeProvider.tsx';
+import { Provider } from 'react-redux';
+import { store } from '@redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<ErrorFallback />}>
-      <LocaleProvider>
-        <App />
-      </LocaleProvider>
+      <Provider store={store}>
+        <LocaleProvider>
+          <App />
+        </LocaleProvider>
+      </Provider>
     </ErrorBoundary>
   </React.StrictMode>
 );
