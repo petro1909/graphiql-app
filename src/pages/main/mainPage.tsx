dbc3a8279446a1d1e0b5b94b04e2d8fb4156d1de
NataliaVozhdaeva <LomzNat@yandex.ru>
2023-12-09 17:12:45 +0400

    feat: add adaptive

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -6,7 +6,7 @@ import classes from './mainPage.module.scss';
 export function MainPage() {
   const { language } = useLocale();
   const placeholder = language.strings.endpointPlaceholder;
-  const isDisabled = true;
+  const isDisabled = false;
   const docBtnStyle = isDisabled ? classes.documentationDisabled : classes.documentationBtn;
   return (
     <main className={classes.main}>
@@ -19,15 +19,13 @@ export function MainPage() {
           {language.strings.doc}
         </div>
       </section>
-      <section className={classes.mainContent}>
-        <section className={classes.sandBox}>
-          <aside className={classes.actions}>actions</aside>
-          <div className={classes.sandBoxRequest}>Query editor / JSON viewer</div>
-          <div className={classes.sandBoxResponce}>Response section</div>
-          <div className={classes.documentationContent}>Documentation content</div>
-          <div className={classes.sandBoxVars}>Variables editor section</div>
-          <div className={classes.sandBoxJson}>Headers editor section</div>
-        </section>
+      <section className={classes.sandBox}>
+        <aside className={classes.actions}>actions</aside>
+        <div className={classes.sandBoxRequest}>Query editor / JSON viewer</div>
+        <div className={classes.sandBoxResponce}>Response section</div>
+        <div className={classes.documentationContent}>Documentation content</div>
+        <div className={classes.sandBoxVars}>Variables editor section</div>
+        <div className={classes.sandBoxJson}>Headers editor section</div>
       </section>
     </main>
   );
