bb12a8f51363e04b6939f9311f9a69015c6d969e
petro1909 <petya-97_97@mail.ru>
2023-12-11 15:40:19 +0300

    refactor: update component styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -4,12 +4,13 @@ import classes from './button.module.scss';
 
 export type ButtonProps = Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'children' | 'type' | 'disabled' | 'onClick'> & {
   mode?: 'dark' | 'light';
+  additionalClass?: string;
 };
 
-export const Button: React.FC<ButtonProps> = ({ children, onClick, type = 'button', disabled, mode = 'dark' }) => {
+export const Button: React.FC<ButtonProps> = ({ children, onClick, type = 'button', disabled, mode = 'dark', additionalClass }) => {
   const buttonStyle = mode === 'dark' ? classes.buttonDark : classes.buttonLight;
   return (
-    <button type={type} className={classNames(classes.button, buttonStyle)} onClick={onClick} disabled={disabled}>
+    <button type={type} className={classNames(classes.button, buttonStyle, additionalClass)} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
