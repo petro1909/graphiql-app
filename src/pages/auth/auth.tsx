b59ecc732cbea08ac707bbf412d5b96814d133a7
Darya <bobko.dv@gmail.com>
2023-12-11 20:30:35 +0100

    feat: auth form layout

---
+++ src/pages/auth/auth.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useLocale } from '@localization/useLocale';
+
+import { Input } from '@components/input/input';
+import { Button } from '@components/button/button';
+
+import classes from './auth.module.scss';
+
+export const AuthorizationPage: React.FC = () => {
+  const { language } = useLocale();
+
+  return (
+    <section className={classes.auth}>
+      <form className={classes.authForm}>
+        <h1 className={classes.title}>GraphiQL</h1>
+        <Input label={language.strings.authEmailLabel} placeholder="Email" />
+        <Input label={language.strings.authPasswordLabel} placeholder="Password" />
+
+        <Button>{language.strings.signIn}</Button>
+      </form>
+    </section>
+  );
+};
