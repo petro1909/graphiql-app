5ba629d932dd0e8096ef714183593ed7243a555f
petro1909 <petya-97_97@mail.ru>
2023-12-12 11:28:56 +0300

    feat: update main page component

--- src/pages/main/mainPage.tsx
+++ src/pages/main/mainPage.tsx
@@ -1,34 +1,63 @@
 import { useLocale } from '@localization/useLocale';
-import { Input } from '@components/input/input';
 import { Button } from '@components/button/button';
-import { ActionsPanel } from '@components/actionsPanel/actionsPanel';
-import { BigSandboxComponent } from '@components/bigSandboxComponent/bigSandboxComponent';
-import { DocumentationSection } from '@components/documentationSection/documentationSection';
-import { SmallSandboxComponent } from '@components/smallSandboxComponent/smallSandboxComponent';
+import { ActionsPanel } from '@components/graphql/actionsPanel/actionsPanel';
+import DocumentationSection from '@components/graphql/documentationSection/documentationSection';
+
 import classes from './mainPage.module.scss';
 import classNames from 'classnames';
+import { EndpointForm } from '@components/graphql/endPointForm/endPointForm';
+import { useEffect, useState } from 'react';
+import { QueryEditor } from '@components/graphql/editor/queryEditor/queryEditor';
+import { QueryPropertiesEditor } from '@components/graphql/editor/queryPropertyEditor/queryPropertyEditor';
+import { SandboxContainer } from '@components/sandboxContainer/sandboxContainer';
+import { Results } from '@components/graphql/results/results';
+import { useSelector } from 'react-redux';
+import { selectDocsEnable } from '@redux/docsSlice';
 
 export function MainPage() {
   const { language } = useLocale();
-  const placeholder = language.strings.endpointPlaceholder;
-  const isDocsShown = false;
-  const docBtnStyle = isDocsShown ? classes.documentationBtn : classes.documentationDisabled;
+  const isDocsEnable = useSelector(selectDocsEnable);
+
+  const [docsVisibility, toggleDocsVisibility] = useState(isDocsEnable);
+
+  useEffect(() => {
+    toggleDocsVisibility(isDocsEnable);
+  }, [isDocsEnable]);
+
   return (
     <main className={classes.main}>
       <section className={classes.mainTop}>
-        <div className={classes.endpoint}>
-          <Input placeholder={placeholder} />
-          <Button mode="light">{language.strings.setEndpoint}</Button>
-        </div>
-        <div className={docBtnStyle}>{language.strings.doc}</div>
+        <EndpointForm />
+        <Button disabled={!isDocsEnable} mode="light" onClick={() => toggleDocsVisibility(!docsVisibility)}>
+          {language.strings.docShowButton}
+        </Button>
       </section>
-      <section className={classNames(classes.sandBox, isDocsShown && classes.visibleDocs)}>
-        <ActionsPanel />
-        <BigSandboxComponent />
-        <BigSandboxComponent />
-        <DocumentationSection />
-        <SmallSandboxComponent />
-        <SmallSandboxComponent />
+      <section className={classes.sandBox}>
+        <section className={classNames(classes.actionsGridWrapper)}>
+          <SandboxContainer>
+            <ActionsPanel />
+          </SandboxContainer>
+        </section>
+        <section className={classNames(classes.queryGridWrapper)}>
+          <SandboxContainer>
+            <QueryEditor />
+          </SandboxContainer>
+        </section>
+        <section className={classNames(classes.queryPropertiesGridWrapper)}>
+          <SandboxContainer>
+            <QueryPropertiesEditor />
+          </SandboxContainer>
+        </section>
+        <section className={classNames(classes.resultsGridWrapper)}>
+          <SandboxContainer>
+            <Results />
+          </SandboxContainer>
+        </section>
+        <section className={classNames(classes.docsGridWrapper, !docsVisibility && classes.docsHidden)}>
+          <SandboxContainer>
+            <DocumentationSection />
+          </SandboxContainer>
+        </section>
       </section>
     </main>
   );
