d01b6d0311b85f191550d6ada881c35277d2c6e2
petro1909 <petya-97_97@mail.ru>
2023-12-12 22:55:55 +0300

    refactor: update prettify function

--- src/components/graphql/editor/baseEditor/editorService/prettify.ts
+++ src/components/graphql/editor/baseEditor/editorService/prettify.ts
@@ -7,19 +7,21 @@ export function prettify(text: string): string {
         .trim()
         .replaceAll(/(\s|\t)+/g, ' ')
         .replaceAll('{', '{\n')
-        .replaceAll('}', '\n}')
+        .replaceAll('}', '\n}\n')
     )
     .filter((line) => line.trim() !== '');
   lines = lines.join('').split('\n');
-  lines = lines.map((line) => {
-    if (line.includes('}')) {
-      --indentLevel;
-    }
-    line = ''.padEnd(indentLevel, '\t').concat(line);
-    if (line.includes('{')) {
-      ++indentLevel;
-    }
-    return line;
-  });
+  lines = lines
+    .map((line) => {
+      if (line.includes('}')) {
+        --indentLevel;
+      }
+      line = ''.padEnd(indentLevel, '\t').concat(line);
+      if (line.includes('{')) {
+        ++indentLevel;
+      }
+      return line;
+    })
+    .filter((line) => line.trim() !== '');
   return lines.join('\n');
 }
