cc3ed128c39f304e46a7b828c25f45ee059956a2
petro1909 <petya-97_97@mail.ru>
2023-12-12 11:39:19 +0300

    feat: create component to manage GraphQl endpoint URL

---
+++ src/components/graphql/endPointForm/endPointForm.tsx
@@ -0,0 +1,27 @@
+import { Input } from '@components/input/input';
+import { Button } from '@components/button/button';
+import { useLocale } from '@localization/useLocale';
+import classes from './endpointForm.module.scss';
+import { store } from '@redux/store';
+import { selectRawRequest, setRawRequest } from '@redux/endpointSlice';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+export function EndpointForm() {
+  const { language } = useLocale();
+  const { URL } = useSelector(selectRawRequest);
+  const [endPoint, setEndpoint] = useState(URL);
+
+  const handleEndpoint = () => {
+    store.dispatch(setRawRequest({ URL: endPoint }));
+  };
+
+  return (
+    <div className={classes.endpoint}>
+      <Input value={endPoint} onChange={(e) => setEndpoint(e.target.value)} placeholder={language.strings.endpointPlaceholder} />
+      <Button mode="light" onClick={handleEndpoint}>
+        {language.strings.setEndpoint}
+      </Button>
+    </div>
+  );
+}
