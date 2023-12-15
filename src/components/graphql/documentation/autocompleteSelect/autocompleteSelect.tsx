a55d63c1126caf260c6704dcf31aac7877dab042
petro1909 <petya-97_97@mail.ru>
2023-12-15 14:46:09 +0300

    feat: create custom autocomplete component

---
+++ src/components/graphql/documentation/autocompleteSelect/autocompleteSelect.tsx
@@ -0,0 +1,77 @@
+import { Input } from '@components/input/input';
+import React from 'react';
+import { useState } from 'react';
+import classes from './autocompleteSelect.module.scss';
+import classNames from 'classnames';
+import { GraphQlEntity } from '@app_types/graphql';
+
+type AutocompleteSelectProps = {
+  items: GraphQlEntity[];
+  placeholder?: string;
+  errorMessage?: string;
+  handleSelectItem: (entity: GraphQlEntity) => void;
+};
+
+export const AutocompleteSelect = ({ items, placeholder, handleSelectItem, errorMessage }: AutocompleteSelectProps) => {
+  const [inputValue, setInputValue] = useState('');
+  const [itemsIsVisible, setItemsIsVisible] = useState(false);
+  const [proposedItems, setProposedItems] = useState(items);
+
+  const filterItems = (value: string) => {
+    value = value.toLowerCase();
+    if (value === '') {
+      return [];
+    }
+
+    return items.filter(
+      (item) => (item.typeName.toLowerCase().toLowerCase().includes(value) && !item.fieldName) || item.fieldName?.toLocaleLowerCase().includes(value)
+    );
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    setItemsIsVisible(true);
+    const filteredItems = filterItems(e.target.value);
+    setProposedItems(filteredItems);
+  };
+
+  const handlerSelectList = (entity: GraphQlEntity) => {
+    setInputValue(entity.fieldName || entity.typeName);
+    setProposedItems(filterItems(entity.fieldName || entity.typeName));
+    setItemsIsVisible(false);
+    handleSelectItem(entity);
+  };
+
+  const handleFocus = () => {
+    if (inputValue !== '') {
+      setItemsIsVisible(true);
+    }
+  };
+
+  const handleBlur = () => {
+    setItemsIsVisible(false);
+  };
+
+  return (
+    <section className={classes.autocompleteWrapper} onBlur={handleBlur}>
+      <div onFocus={handleFocus}>
+        <Input placeholder={placeholder} value={inputValue} onChange={handleChange} error={errorMessage} className={classes.autocompleteInput} />
+      </div>
+      <section className={classNames(classes.itemsWrapper, itemsIsVisible && classes.visible)}>
+        <section className={classes.itemsScrollWrapper}>
+          {proposedItems.map((proposedItem, index) => (
+            <p className={classes.proposedItem} key={index} onMouseDown={() => handlerSelectList(proposedItem)}>
+              <span>{proposedItem.typeName}</span>
+              {proposedItem.fieldName && (
+                <>
+                  <span>.</span>
+                  <span>{proposedItem.fieldName}</span>
+                </>
+              )}
+            </p>
+          ))}
+        </section>
+      </section>
+    </section>
+  );
+};
