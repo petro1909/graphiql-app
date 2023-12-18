4171a4a8dd7f8bfa57d30fadd487eead2b08f18f
petro1909 <petya-97_97@mail.ru>
2023-12-18 22:21:56 +0300

    refactor: move editor functions to hook

--- src/components/graphql/editor/baseEditor/baseEditor.tsx
+++ src/components/graphql/editor/baseEditor/baseEditor.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useRef } from 'react';
 import classes from './baseEditor.module.scss';
 import { EditorLine } from './editorLine/editorLine';
+import { useEditor } from './editorHooks/useEditor';
 
 type BaseEditorProps = {
   initText: string;
@@ -11,55 +12,7 @@ type BaseEditorProps = {
 
 export function BaseEditor({ initText, handleChangeText, isDisabled = false, isLinesDisabled = false }: BaseEditorProps) {
   const ref = useRef<HTMLTextAreaElement>(null);
-  const [text, setText] = useState(initText);
-  const [cursor, setCursor] = useState(0);
-
-  const rows = useMemo(() => {
-    return text.split('\n').length;
-  }, [text]);
-
-  const currentRow = useMemo(() => {
-    const textBefore = text.slice(0, ref.current?.selectionEnd);
-    const textArr = textBefore.split('\n');
-
-    return textArr.length;
-  }, [cursor]);
-
-  useEffect(() => {
-    setText(initText);
-  }, [initText]);
-
-  useEffect(() => {
-    const input = ref.current;
-    if (input) input.setSelectionRange(cursor, cursor);
-    handleChangeText && handleChangeText(text);
-  }, [ref, cursor, text]);
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    const target = e.currentTarget;
-    if (e.code === 'Tab') {
-      e.preventDefault();
-      target.setRangeText('\t', target.selectionStart, target.selectionStart, 'end');
-    }
-  };
-
-  const handleKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    const target = e.currentTarget;
-    if (e.key === '{' || e.key === '[') {
-      const symbol = e.key === '{' ? '}' : ']';
-      target.setRangeText(symbol, target.selectionStart, target.selectionStart, 'preserve');
-    }
-  };
-
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setCursor(e.target.selectionStart);
-    setText(e.target.value);
-  };
-
-  const handleClick = (e: React.MouseEvent<HTMLTextAreaElement, MouseEvent>) => {
-    setText(e.currentTarget.value);
-    setCursor(e.currentTarget.selectionEnd);
-  };
+  const { text, rows, currentRow, handleKeyDown, handleKeyUp, handleChange, handleClick } = useEditor(initText, ref, handleChangeText);
 
   return (
     <section className={classes.editorWrapper}>
---
+++ src/components/graphql/editor/baseEditor/editorHooks/useEditor.ts
@@ -0,0 +1,63 @@
+import { useState, useMemo, useEffect } from 'react';
+
+const tab: string = '\t';
+const openSquareBraces = '[';
+const closeSquareBraces = ']';
+const openCurlyBraces = '{';
+const closeCurlyBraces = '}';
+const newLine: string = '\n';
+const keyTab: string = 'Tab';
+
+export const useEditor = (initText: string, ref: React.RefObject<HTMLTextAreaElement>, handleChangeText?: (text: string) => void) => {
+  const [text, setText] = useState(initText);
+  const [cursor, setCursor] = useState(0);
+
+  const rows = useMemo(() => {
+    return text.split(newLine).length;
+  }, [text]);
+
+  const currentRow = useMemo(() => {
+    const textBefore = text.slice(0, ref.current?.selectionEnd);
+    const textArr = textBefore.split(newLine);
+
+    return textArr.length;
+  }, [cursor]);
+
+  useEffect(() => {
+    setText(initText);
+  }, [initText]);
+
+  useEffect(() => {
+    const input = ref.current;
+    if (input) input.setSelectionRange(cursor, cursor);
+    handleChangeText && handleChangeText(text);
+  }, [ref, cursor, text]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const target = e.currentTarget;
+    if (e.code === keyTab) {
+      e.preventDefault();
+      target.setRangeText(tab, target.selectionStart, target.selectionStart, 'end');
+    }
+  };
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const target = e.currentTarget;
+    if (e.key === openCurlyBraces || e.key === openSquareBraces) {
+      const symbol = e.key === openCurlyBraces ? closeCurlyBraces : closeSquareBraces;
+      target.setRangeText(symbol, target.selectionStart, target.selectionStart, 'preserve');
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCursor(e.target.selectionStart);
+    setText(e.target.value);
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLTextAreaElement, MouseEvent>) => {
+    setText(e.currentTarget.value);
+    setCursor(e.currentTarget.selectionEnd);
+  };
+
+  return { text, setText, cursor, rows, currentRow, handleKeyDown, handleKeyUp, handleChange, handleClick };
+};
